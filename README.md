fde37dbbf20a8744fe6b1c3e121c4549b7fd33a2
Evan Dobson <evan.dobson18@gmail.com>
2022-07-07 12:39:41 -0500

    Rework

--- README.md
+++ README.md
@@ -13,35 +13,29 @@ static inline float fast_tanf           (float x);
 static inline float fast_sqrtf          (float x);
 
 // Common
-static inline void  TN_copy             (T out[N], T a[N]);
-static inline void  TN_add              (T out[N], T a[N], T b[N]);
-static inline void  TN_sub              (T out[N], T a[N], T b[N]);
-static inline void  TN_mul              (T out[N], T a[N], T b[N]);
+static inline void  vecN_copy           (T out[N], T a[N]);
+static inline void  vecN_add            (T out[N], T a[N], T b[N]);
+static inline void  vecN_sub            (T out[N], T a[N], T b[N]);
+static inline void  vecN_mul            (T out[N], T a[N], T b[N]);
 
 // Vector
-static inline void  TN_scale            (T out[N], T a[N], T b);
-static inline T     TN_dot              (T a[N], T b[N]);
-static inline T     TN_len2             (T a[N]);
-static inline T     TN_len              (T a[N]);
-static inline void  TN_norm             (T out[N], T a[N]);
+static inline void  vecN_scale          (T out[N], T a[N], T b);
+static inline T     vecN_dot            (T a[N], T b[N]);
+static inline T     vecN_len2           (T a[N]);
+static inline T     vecN_len            (T a[N]);
+static inline void  vecN_norm           (T out[N], T a[N]);
 
 // Vector specific
-static inline void  T3_cross            (T out[3], T a[3], T b[3]);
+static inline void  vec3_cross          (T out[3], T a[3], T b[3]);
 
 // Matrix specific
-static inline void  T9_copy             (T out[9], T a[9]);
-static inline void  T9_identity         (T out[9]);
-static inline void  T9_mul              (T out[9], T a[9], T b[9]);
-static inline void  T9_translate        (T out[9], T translation[3]);
-static inline void  T9_rotate           (T out[9], T axis[3], T angle);
-static inline void  T9_scale            (T out[9], T scale[3]);
-static inline void  T16_copy            (T out[16], T a[16]);
-static inline void  T16_identity        (T out[16]);
-static inline void  T16_mul             (T out[16], T a[16], T b[16]);
-static inline void  T16_translate       (T out[16], T translation[3]);
-static inline void  T16_rotate          (T out[16], T axis[3], T angle);
-static inline void  T16_scale           (T out[16], T scale[3]);
-static inline void  T16_lookat          (T out[16], T eye[3], T target[3], T up[3]);
-static inline void  T16_perspective     (T out[16], float fov, float aspect, float near, float far);
-static inline void  T16_ortho           (T out[16], float bottom, float top, float left, float right, float near, float far);
+static inline void  mat4_copy           (T out[4][4], T a[4][4]);
+static inline void  mat4_identity       (T out[4][4]);
+static inline void  mat4_mul            (T out[4][4], T a[4][4], T b[4][4]);
+static inline void  mat4_translate      (T out[4][4], T translation[3]);
+static inline void  mat4_rotate         (T out[4][4], T axis[3], T angle);
+static inline void  mat4_scale          (T out[4][4], T scale[3]);
+static inline void  mat4_lookat         (T out[4][4], T eye[3], T target[3], T up[3]);
+static inline void  mat4_perspective    (T out[4][4], float fov, float aspect, float near, float far);
+static inline void  mat4_ortho          (T out[4][4], float bottom, float top, float left, float right, float near, float far);
 ```
--- math.h
+++ math.h
@@ -17,139 +17,98 @@ static inline float fast_sqrtf(float x) {
     return (x / z + z) * 0.5f;
 }
 
-#define MATH_DEFINE_COMMON(T, n)\
-static inline void T##n##_copy(T out[n], T a[n])        { for (int i = 0; i < n; i++) out[i] = a[i];        }\
-static inline void T##n##_add(T out[n], T a[n], T b[n]) { for (int i = 0; i < n; i++) out[i] = a[i] + b[i]; }\
-static inline void T##n##_sub(T out[n], T a[n], T b[n]) { for (int i = 0; i < n; i++) out[i] = a[i] - b[i]; }\
-static inline void T##n##_mul(T out[n], T a[n], T b[n]) { for (int i = 0; i < n; i++) out[i] = a[i] * b[i]; }
+#define MATH_DEFINE_VECTOR(n)\
+static inline void  vec##n##_copy(float out[n], float a[n])             { for (int i = 0; i < n; i++) out[i] = a[i];        }\
+static inline void  vec##n##_add(float out[n], float a[n], float b[n])  { for (int i = 0; i < n; i++) out[i] = a[i] + b[i]; }\
+static inline void  vec##n##_sub(float out[n], float a[n], float b[n])  { for (int i = 0; i < n; i++) out[i] = a[i] - b[i]; }\
+static inline void  vec##n##_mul(float out[n], float a[n], float b[n])  { for (int i = 0; i < n; i++) out[i] = a[i] * b[i]; }\
+static inline void  vec##n##_scale(float out[n], float a[n], float b)   { for (int i = 0; i < n; i++) out[i] = a[i] * b;    }\
+static inline float vec##n##_dot(float a[n], float b[n])                { float d = 0.f; for (int i = 0; i < n; i++) d += a[i] * b[i]; return d; }\
+static inline float vec##n##_len2(float a[n])                           { return vec##n##_dot(a, a); }\
+static inline float vec##n##_len(float a[n])                            { return fast_sqrtf(vec##n##_len2(a)); }\
+static inline void  vec##n##_norm(float out[n], float a[n])             { vec##n##_scale(out, a, 1.f / vec##n##_len(a)); }
 
-#define MATH_DEFINE_VECTOR(T, n)\
-static inline void T##n##_scale(T out[n], T a[n], T b)  { for (int i = 0; i < n; i++) out[i] = a[i] * b; }\
-static inline T    T##n##_dot(T a[n], T b[n])           { float d = 0.f; for (int i = 0; i < n; i++) d += a[i] * b[i]; return d; }\
-static inline T    T##n##_len2(T a[n])                  { return T##n##_dot(a, a); }\
-static inline T    T##n##_len(T a[n])                   { return fast_sqrtf(T##n##_len2(a)); }\
-static inline void T##n##_norm(T out[n], T a[n])        { T##n##_scale(out, a, 1.f / T##n##_len(a)); }
+MATH_DEFINE_VECTOR(2)
+MATH_DEFINE_VECTOR(3)
+MATH_DEFINE_VECTOR(4)
 
-#define MATH_DEFINE_VECTOR_SPECIFIC(T)\
-static inline void T##2_rotate(T out[2], T a[2], T angle) {\
-    T c = fast_cosf(angle), s = fast_sinf(angle);\
-    out[0] = a[0] * c - a[1] * s;\
-    out[1] = a[0] * s + a[1] * c;\
-}\
-static inline void T##3_cross(T out[3], T a[3], T b[3]) {\
-    out[0] = a[1] * b[2] - a[2] * b[1];\
-    out[1] = a[2] * b[0] - a[0] * b[2];\
-    out[2] = a[0] * b[1] - a[1] * b[0];\
+static inline void vec3_cross(float out[3], float a[3], float b[3]) {
+    out[0] = a[1] * b[2] - a[2] * b[1];
+    out[1] = a[2] * b[0] - a[0] * b[2];
+    out[2] = a[0] * b[1] - a[1] * b[0];
 }
-
-#define MATH_DEFINE_MATRIX_SPEICIFIC(T)\
-static inline void T##9_identity(T out[9]) {\
-    T##9_copy(out, (T [9]){[0] = 1.f, [4] = 1.f, [8] = 1.f});\
-}\
-static inline void T##9_translate(T out[9], T translation[2]) {\
-    T##9_identity(out);\
-    out[6] = translation[0];\
-    out[7] = translation[1];\
-}\
-static inline void T##9_rotate(T out[9], T angle) {\
-    T s = fast_sinf(angle), c = fast_cosf(angle);\
-    T##9_copy(out, (T [9]){[0] = c, [1] = -s, [3] = s, [4] = c});\
-}\
-static inline void T##9(T out[9], T scale[3]) {\
-    T##9_identity(out);\
-    out[0] = scale[0];\
-    out[4] = scale[1];\
-}\
-static inline void T##16_identity(T out[16]) {\
-    T##16_copy(out, (T [16]){[0] = 1.f, [5] = 1.f, [10] = 1.f, [15] = 1.f});\
-}\
-static inline void T##16_translate(T out[16], T translation[3]) {\
-    T##16_identity(out);\
-    out[12] = translation[0];\
-    out[13] = translation[1];\
-    out[14] = translation[2];\
-}\
-static inline void T##16_rotate(T out[16], T axis[3], T angle) {\
-    T c = fast_cosf(angle);\
-    T an[3], at[3], as[3];\
-    T##3_norm(an, axis);\
-    T##3_scale(at, an, 1.f - c);\
-    T##3_scale(as, an, fast_sinf(angle));\
-    T##16_copy(out, (T [16]){\
-        an[0] * at[0] + c,     an[1] * at[0] + as[2], an[2] * at[0] - as[1], 0.f,\
-        an[0] * at[1] - as[2], an[1] * at[1] + c,     an[2] * at[1] + as[0], 0.f,\
-        an[0] * at[2] + as[2], an[1] * at[2] + as[0], an[2] * at[2] + c,     0.f,\
-        0.f,                   0.f,                   0.f,                   1.f\
-    });\
-}\
-static inline void T##16_scale(T out[16], T scale[3]) {\
-    T##16_identity(out);\
-    out[ 0] = scale[0];\
-    out[ 5] = scale[1];\
-    out[10] = scale[2];\
-}\
-static inline void T##16_lookat(T out[16], T eye[3], T target[3], T up[3]) {\
-    T f[3], r[3], u[3];\
-    T##3_sub(f, target, eye);\
-    T##3_norm(f, f);\
-    T##3_cross(r, f, up);\
-    T##3_norm(r, r);\
-    T##3_cross(u, r, f);\
-    T##16_copy(out, (T [16]){\
-        r[0],                 u[0],               -f[0],               0.0f,\
-        r[1],                 u[1],               -f[1],               0.0f,\
-        r[2],                 u[2],               -f[2],               0.0f,\
-        -T##3_dot(r, eye), -T##3_dot(u, eye), -T##3_dot(f, eye), 1.0f\
-    });\
-}\
-static inline void T##16_perspective(T out[16], T fov, T aspect, T near, T far) {\
-    T nf  = 1.f / (near - far);\
-    fov = 1.f / fast_tanf(fov * 0.5f);\
-    T##16_copy(out, (T [16]){\
-        [ 0] = fov / aspect,\
-        [ 5] = fov,\
-        [10] = (near + far) * nf,\
-        [11] = -1.f,\
-        [15] = 2.f * near * far * nf\
-    });\
-}\
-static inline void T##16_ortho(T out[16], T bottom, T top, T left, T right, T near, T far) {\
-    T rl =  1.f / (right - bottom);\
-    T tb =  1.f / (top - bottom);\
-    T fn = -1.f / (far - near);\
-    T##16_copy(out, (T [16]){\
-        [ 0] = 2.f * rl,\
-        [ 5] = 2.f * tb,\
-        [10] = 2.f * fn,\
-        [12] = -(right + left) * rl,\
-        [13] = -(top + bottom) * tb,\
-        [14] = -(far + near) * fn,\
-        [15] = 1.f\
-    });\
-}\
-
-MATH_DEFINE_COMMON(int,     2)
-MATH_DEFINE_COMMON(float,   2)
-MATH_DEFINE_COMMON(float,   3)
-MATH_DEFINE_COMMON(float,   4)
-MATH_DEFINE_COMMON(float,   9)
-MATH_DEFINE_COMMON(float,  16)
-MATH_DEFINE_COMMON(double,  2)
-MATH_DEFINE_COMMON(double,  3)
-MATH_DEFINE_COMMON(double,  4)
-MATH_DEFINE_COMMON(double,  9)
-MATH_DEFINE_COMMON(double, 16)
-
-MATH_DEFINE_VECTOR(int,     2)
-MATH_DEFINE_VECTOR(float,   2)
-MATH_DEFINE_VECTOR(float,   3)
-MATH_DEFINE_VECTOR(float,   4)
-MATH_DEFINE_VECTOR(double,  2)
-MATH_DEFINE_VECTOR(double,  3)
-MATH_DEFINE_VECTOR(double,  4)
-
-MATH_DEFINE_VECTOR_SPECIFIC(float)
-MATH_DEFINE_VECTOR_SPECIFIC(double)
-
-MATH_DEFINE_MATRIX_SPEICIFIC(float)
-MATH_DEFINE_MATRIX_SPEICIFIC(double)
+static inline void mat4_copy(float out[4][4], float a[4][4]) {
+    for (int i = 0; i < 4; i++)
+        vec4_copy(out[i], a[i]);
+}
+static inline void mat4_identity(float out[4][4]) {
+    mat4_copy(out, (float [4][4]){[0][0] = 1.f, [1][1] = 1.f, [2][2] = 1.f, [3][3] = 1.f});
+}
+static inline void mat4_mul(float out[4][4], float a[4][4], float b[4][4]) {
+    for (int i = 0; i < 4; i++)
+        vec4_mul(out[i], a[i], b[i]);
+}
+static inline void mat4_translate(float out[4][4], float translation[3]) {
+    mat4_identity(out);
+    out[3][0] = translation[0];
+    out[3][1] = translation[1];
+    out[3][2] = translation[2];
+}
+static inline void mat4_rotate(float out[4][4], float axis[3], float angle) {
+    float c = fast_cosf(angle);
+    float an[3], at[3], as[3];
+    vec3_norm(an, axis);
+    vec3_scale(at, an, 1.f - c);
+    vec3_scale(as, an, fast_sinf(angle));
+    mat4_copy(out, (float [4][4]){
+        an[0] * at[0] + c,     an[1] * at[0] + as[2], an[2] * at[0] - as[1], 0.f,
+        an[0] * at[1] - as[2], an[1] * at[1] + c,     an[2] * at[1] + as[0], 0.f,
+        an[0] * at[2] + as[2], an[1] * at[2] + as[0], an[2] * at[2] + c,     0.f,
+        0.f,                   0.f,                   0.f,                   1.f
+    });
+}
+static inline void mat4_scale(float out[4][4], float scale[3]) {
+    mat4_identity(out);
+    out[0][0] = scale[0];
+    out[1][1] = scale[1];
+    out[2][2] = scale[2];
+}
+static inline void mat4_lookat(float out[4][4], float eye[3], float target[3], float up[3]) {
+    float f[3], r[3], u[3];
+    vec3_sub(f, target, eye);
+    vec3_norm(f, f);
+    vec3_cross(r, f, up);
+    vec3_norm(r, r);
+    vec3_cross(u, r, f);
+    mat4_copy(out, (float [4][4]){
+        {r[0], u[0], -f[0], 0.0f},
+        {r[1], u[1], -f[1], 0.0f},
+        {r[2], u[2], -f[2], 0.0f},
+        {-vec3_dot(r, eye), -vec3_dot(u, eye), -vec3_dot(f, eye), 1.0f}
+    });
+}
+static inline void mat4_perspective(float out[4][4], float fov, float aspect, float near, float far) {
+    float nf  = 1.f / (near - far);
+    fov = 1.f / fast_tanf(fov * 0.5f);
+    mat4_copy(out, (float [4][4]){
+        [0][0] = fov / aspect,
+        [1][1] = fov,
+        [2][2] = (near + far) * nf,
+        [2][3] = -1.f,
+        [3][2] = 2.f * near * far * nf
+    });
+}
+static inline void mat4_ortho(float out[4][4], float bottom, float top, float left, float right, float near, float far) {
+    float rl =  1.f / (right - bottom);
+    float tb =  1.f / (top - bottom);
+    float fn = -1.f / (far - near);
+    mat4_copy(out, (float [4][4]){
+        [0][0] = 2.f * rl,
+        [1][1] = 2.f * tb,
+        [2][2] = 2.f * fn,
+        [3][0] = -(right + left) * rl,
+        [3][1] = -(top + bottom) * tb,
+        [3][2] = -(far + near) * fn,
+        [3][3] = 1.f
+    });
+}
